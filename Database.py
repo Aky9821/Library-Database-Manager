b59db3bbeac40ef5e85517e1bc5bab29bd997937
Akshay <akshay..nagar0101@outlook.com>
2021-11-29 17:53:56 +0530

    .exe Executable build

--- Database.py
+++ Database.py
@@ -4,53 +4,74 @@ import sqlite3
 def connectToDatabase():
     connection = sqlite3.connect("Books.db")
     cur = connection.cursor()
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS Book(SNo INTEGER PRIMARY KEY, Title VARCHAR, Author  VARCHAR, Year INTEGER, "
-        "UID INTEGER UNIQUE)")
+  
+    # cur.execute(
+    #    "CREATE TABLE IF NOT EXISTS 'Bx-Books' (ISBN VARCHAR(13) PRIMARY KEY, Title varchar(255),Author varchar(255), "
+    #    "Year INT(10), Publisher varchar(255), ImageURLS varchar(255),ImageURLM varchar(255),"
+    #    "ImageURLL varchar(255)) "
+    # )
+    # sqlFile = open("BX-Books.sql")
+    # sqlAsString = sqlFile.read()
+    # cur.executescript(sqlAsString)
+
+    # cur.execute("ALTER TABLE 'Bx-Books' RENAME TO Book")
+
     connection.commit()
     connection.close()
 
 
-def insert(title, author, year, UID):
+def insert(title, author, year, ISBN):
     connection = sqlite3.connect("Books.db")
     cur = connection.cursor()
-    cur.execute("INSERT INTO Book VALUES(NULL,?,?,?,?)", (title, author, year, UID))
+    cur.execute("INSERT INTO Book VALUES(?,?,?,?,'')", (ISBN, title, author, year))
     connection.commit()
     connection.close()
 
 
-def search(title="", author="", year="", UID=""):
+def search(title="", author="", year="", ISBN=""):
+    title = '%' + title + '%'
+    author = '%' + author + '%'
+    year = '%' + year + '%'
+    ISBN = '%' + ISBN + '%'
     connection = sqlite3.connect("Books.db")
     cur = connection.cursor()
 
-    if title != "" and author != "" and year != "" and UID != "":
-        cur.execute("SELECT * FROM Book WHERE title=? AND author=? AND year=? AND UID=?", (title, author, year, UID))
+    if title != "" and author != "" and year != "" and ISBN != "":
+        cur.execute(
+            "SELECT * FROM Book WHERE UPPER(title)LIKE ? AND UPPER(author) LIKE ? AND year LIKE ? AND ISBN LIKE?",
+            (title, author, year, ISBN))
     elif (
-            title != "" and author == "" and year == "" and UID == "") or (
-            title == "" and author != "" and year == "" and UID == "") or (
-            title == "" and author == "" and year != "" and UID == "") or (
-            title == "" and author == "" and year == "" and UID != ""):
-        cur.execute("SELECT * FROM Book WHERE title=? OR author=? OR year=? OR UID=?", (title, author, year, UID))
-    elif title != "" and author != "" and year == "" and UID == "":
-        cur.execute("SELECT * FROM Book WHERE title=? AND author=? ", (title, author))
-    elif title != "" and author == "" and year != "" and UID == "":
-        cur.execute("SELECT * FROM Book WHERE title=?  AND year=?", (title, year))
-    elif title != "" and author == "" and year == "" and UID != "":
-        cur.execute("SELECT * FROM Book WHERE title=? AND UID=?", (title, UID))
-    elif title == "" and author != "" and year != "" and UID == "":
-        cur.execute("SELECT * FROM Book WHERE author=? AND year=? ", (author, year))
-    elif title == "" and author != "" and year == "" and UID != "":
-        cur.execute("SELECT * FROM Book WHERE  author=? AND UID=?", (author, UID))
-    elif title == "" and author == "" and year != "" and UID != "":
-        cur.execute("SELECT * FROM Book WHERE year=? AND UID=?", (year, UID))
-    elif title != "" and author != "" and year != "" and UID == "":
-        cur.execute("SELECT * FROM Book WHERE title=? AND author=? AND year=?", (title, author, year))
-    elif title != "" and author != "" and year == "" and UID != "":
-        cur.execute("SELECT * FROM Book WHERE title=? AND author=?  AND UID=?", (title, author, UID))
-    elif title != "" and author == "" and year != "" and UID != "":
-        cur.execute("SELECT * FROM Book WHERE title=?  AND year=? AND UID=?", (title, year, UID))
-    elif title == "" and author != "" and year != "" and UID != "":
-        cur.execute("SELECT * FROM Book WHERE author=? AND year=? AND UID=?", (author, year, UID))
+            title != "" and author == "" and year == "" and ISBN == "") or (
+            title == "" and author != "" and year == "" and ISBN == "") or (
+            title == "" and author == "" and year != "" and ISBN == "") or (
+            title == "" and author == "" and year == "" and ISBN != ""):
+        cur.execute(
+            "SELECT * FROM Book WHERE UPPER(title) LIKE ? OR UPPER(author) LIKE ? OR year LIKE ? OR ISBN LIKE ?",
+            (title, author, year, ISBN) + '%')
+    elif title != "" and author != "" and year == "" and ISBN == "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(title) LIKE ? AND UPPER(author) LIKE ? ", (title, author))
+    elif title != "" and author == "" and year != "" and ISBN == "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(title) LIKE ?  AND year LIKE?", (title, year))
+    elif title != "" and author == "" and year == "" and ISBN != "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(title) LIKE ? AND ISBN LIKE ?", (title, ISBN))
+    elif title == "" and author != "" and year != "" and ISBN == "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(author)LIKE ? AND year LIKE ? ", (author, year))
+    elif title == "" and author != "" and year == "" and ISBN != "":
+        cur.execute("SELECT * FROM Book WHERE  UPPER(author) LIKE ? AND ISBN LIKE ?", (author, ISBN))
+    elif title == "" and author == "" and year != "" and ISBN != "":
+        cur.execute("SELECT * FROM Book WHERE year LIKE ? AND ISBN LIKE ?", (year, ISBN))
+    elif title != "" and author != "" and year != "" and ISBN == "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(title) LIKE ? AND UPPER(author) LIKE ? AND year LIKE ?",
+                    (title, author, year))
+    elif title != "" and author != "" and year == "" and ISBN != "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(title) LIKE ? AND UPPER(author) LIKE ?  AND ISBN LIKE ?",
+                    (title, author, ISBN))
+    elif title != "" and author == "" and year != "" and ISBN != "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(title) LIKE ?  AND year LIKE ? AND ISBN LIKE ?",
+                    (title, year, ISBN))
+    elif title == "" and author != "" and year != "" and ISBN != "":
+        cur.execute("SELECT * FROM Book WHERE UPPER(author) LIKE ? AND year LIKE ? AND ISBN LIKE ?",
+                    (author, year, ISBN))
     rows = cur.fetchall()
     connection.close()
     return rows
@@ -65,27 +86,20 @@ def viewAll():
     return rows
 
 
-def delete(SNo):
+def delete(ISBN):
     connection = sqlite3.connect("Books.db")
     cur = connection.cursor()
-    cur.execute("DELETE FROM book where SNo=?", (SNo,))
+    cur.execute("DELETE FROM book where ISBN=?", (ISBN,))
     connection.commit()
     connection.close()
 
 
-def update(SNo, title, author, year, UID):
+def update(title, author, year, ISBNnew, ISBNold):
     connection = sqlite3.connect("Books.db")
     cur = connection.cursor()
-    cur.execute("UPDATE Book SET title=?, author=?,year=?,UID=? WHERE SNo=?", (title, author, year, UID, SNo))
+    cur.execute("UPDATE Book SET title=?, author=?,year=?,ISBN=? WHERE ISBN=?", (title, author, year, ISBNnew, ISBNold))
     connection.commit()
     connection.close()
 
 
 connectToDatabase()
-delete(6)
-'''print(search(year=2000, UID=5678, title='Six of crows'))
-print(viewAll())
-delete(4)
-
-print(viewAll())
-'''
--- GUI.py
+++ GUI.py
@@ -12,29 +12,30 @@ def viewFunction():
     # booklist.insert(END, "SNo   Title      Author   Year UID")
     for x in Database.viewAll():
         booklist.insert(END, x)
+    scrollFunction()
 
 
 def searchFunction():
     booklist.delete(0, END)
     flag = 0
-    for x in Database.search(title.get(), author.get(), year.get(), UID.get()):
+    for x in Database.search(title.get().upper(), author.get().upper(), year.get(), UID.get()):
         flag = 1
         booklist.insert(END, x)
     if flag == 0:
-        booklist.insert(END, 'Entered Search Not Found!')
+        booklist.insert(END, 'Entered Search Not Found.')
 
 
 def addFunction():
     booklist.delete(0, END)
     flag = 0
-    for x in Database.search(UID=UID.get()):
+    for x in Database.search(ISBN=UID.get()):
         booklist.insert(END, x)
         flag = 1
     if (title.get() == '' or author.get() == '' or year.get() == '' or UID.get() == ''):
-        booklist.insert(END, 'All entries are mandatory and can\'t be left blank!')
+        booklist.insert(END, 'All entries are mandatory and can\'t be left blank.')
     elif flag == 1:
-        booklist.insert(END, 'UID already present in database!')
-        booklist.insert(END, 'All UID should be unique.')
+        booklist.insert(END, 'ISBN already present in database.')
+        booklist.insert(END, 'ISBN should be unique. Please try again.')
     else:
         Database.insert(title.get(), author.get(), year.get(), UID.get())
         booklist.delete(0, END)
@@ -43,26 +44,90 @@ def addFunction():
         booklist.insert(END, 'Entry Added!')
 
 
+def deleteFunction():
+    if selectedRow[0] != -999:
+        Database.delete(selectedRow[0])
+        booklist.delete(0, END)
+        booklist.insert(END, selectedRow)
+        booklist.insert(END, "\nEntry has been deleted.")
+
+
+def updateFunction():
+    if selectedRow[0] != -999:
+        if entryUID.get() == selectedRow[0]:
+            Database.update(entryTitle.get(), entryAuthor.get(), entryYear.get(), entryUID.get(), entryUID.get())
+            booklist.delete(0, END)
+            searchFunction()
+            booklist.insert(END, "Entry Updated!")
+        else:
+            booklist.delete(0, END)
+            flag = 0
+
+            for x in Database.search(ISBN=UID.get()):
+                booklist.insert(END, x)
+                flag = 1
+            if flag == 1:
+                booklist.insert(END, 'ISBN already present in database.')
+                booklist.insert(END, 'ISBN should be unique. Please try again.')
+            else:
+                Database.update(entryTitle.get(), entryAuthor.get(), entryYear.get(), entryUID.get(), selectedRow[0])
+                booklist.delete(0, END)
+                searchFunction()
+                booklist.insert(END, "Entry Updated.")
+
+
+def exitFunction():
+    window.destroy()
+
+
+def getSelectedRow(event):
+    index = booklist.curselection()[0]
+    global selectedRow
+    selectedRow = booklist.get(index)
+    entryAuthor.delete(0, END)
+    entryTitle.delete(0, END)
+    entryUID.delete(0, END)
+    entryYear.delete(0, END)
+    entryTitle.insert(END, selectedRow[1])
+    entryAuthor.insert(END, selectedRow[2])
+    entryYear.insert(END, selectedRow[3])
+    entryUID.insert(END, selectedRow[0])
+
+
+def scrollFunction():
+    scrollbarY = Scrollbar(window, bg=rgb_hack((50, 50, 50)))
+    scrollbarY.grid(column=4, row=0, rowspan=9, sticky='ns', padx=10, pady=10)
+    booklist.configure(yscrollcommand=scrollbarY.set)
+    scrollbarY.configure(command=booklist.yview)
+
+    scrollbarX = Scrollbar(window, bg=rgb_hack((50, 50, 50)), orient='horizontal')
+    scrollbarX.grid(column=3, row=10, sticky='ew', padx=10, pady=10)
+    booklist.configure(xscrollcommand=scrollbarX.set)
+    scrollbarX.configure(command=booklist.xview)
+
+
 window = Tk()
 window.title("Book Store")
+window.state('zoomed')
 window.config(bg=rgb_hack((50, 50, 50)))
 myFont = ("Consolas", "16", "bold")
 
-labelTitle = Label(window, text="Book Title", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
+labelTitle = Label(window, text="Book Title", width=10, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                    fg=rgb_hack((222, 222, 222)))
 labelTitle.grid(row=0, column=0, padx=10, pady=10)
-labelAuthor = Label(window, text="Author", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
+labelAuthor = Label(window, text="Author", width=10, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                     fg=rgb_hack((222, 222, 222)))
 labelAuthor.grid(row=1, column=0, padx=10, pady=10)
-labelYear = Label(window, text="Year", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
+labelYear = Label(window, text="Year", width=10, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                   fg=rgb_hack((222, 222, 222)))
 labelYear.grid(column=0, row=2, padx=10, pady=10)
-labelUID = Label(window, text="Unique ID", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
+labelUID = Label(window, text="ISBN", width=10, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                  fg=rgb_hack((222, 222, 222)))
 labelUID.grid(column=0, row=3, padx=10, pady=10)
 
 title = StringVar()
-entryTitle = Entry(window, textvariable=title, width=20, borderwidth=2, font=myFont, bg=rgb_hack((50, 50, 50)),
+entryTitle = Entry(window, textvariable=title, width=20, borderwidth=2, font=myFont,
+                   bg=rgb_hack((50, 50, 50)),
                    fg=rgb_hack((222, 222, 222)), relief="ridge")
 entryTitle.grid(column=1, row=0, padx=10, pady=10, ipady=11)
 
@@ -81,15 +146,12 @@ entryUID = Entry(window, textvariable=UID, width=20, borderwidth=2, font=myFont,
                  fg=rgb_hack((222, 222, 222)), relief="ridge")
 entryUID.grid(column=1, row=3, padx=10, pady=10, ipady=11)
 
-booklist = Listbox(window, height=20, width=50, font=myFont, bg=rgb_hack((50, 50, 50)),
+booklist = Listbox(window, height=28, width=65, font=myFont, bg=rgb_hack((50, 50, 50)),
                    fg=rgb_hack((217, 217, 217)), borderwidth=2,
                    relief="ridge")
-booklist.grid(column=3, row=0, rowspan=6, padx=10, pady=10)
+booklist.grid(column=3, row=0, rowspan=9, padx=10, pady=10)
 
-scrollbar = Scrollbar(window, bg=rgb_hack((50, 50, 50)))
-scrollbar.grid(column=4, row=0, rowspan=6)
-booklist.configure(yscrollcommand=scrollbar.set)
-scrollbar.configure(command=booklist.yview)
+booklist.bind('<<ListboxSelect>>', getSelectedRow)
 
 buttonViewAll = Button(window, text="View All", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                        fg=rgb_hack((217, 217, 217)), activebackground=rgb_hack((49, 131, 212)), borderwidth=2,
@@ -109,17 +171,20 @@ buttonAddEntry.grid(column=5, row=2, padx=10, pady=10)
 
 buttonUpdate = Button(window, text="Update Selected", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                       fg=rgb_hack((217, 217, 217)), activebackground=rgb_hack((49, 131, 212)), borderwidth=2,
-                      relief="ridge")
+                      relief="ridge", command=updateFunction)
 buttonUpdate.grid(column=5, row=3, padx=10, pady=10)
 
 buttonDelete = Button(window, text="Delete Selected", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                       fg=rgb_hack((217, 217, 217)), activebackground=rgb_hack((49, 131, 212)), borderwidth=2,
-                      relief="ridge")
+                      relief="ridge", command=deleteFunction)
 buttonDelete.grid(column=5, row=4, padx=10, pady=10)
 
 buttonExit = Button(window, text="Exit", width=20, height=2, font=myFont, bg=rgb_hack((50, 50, 50)),
                     fg=rgb_hack((217, 217, 217)), activebackground=rgb_hack((49, 131, 212)), borderwidth=2,
-                    relief="ridge")
+                    relief="ridge", command=exitFunction)
 buttonExit.grid(column=5, row=5, padx=10, pady=10)
 
+selectedRow = (-999,)
+
+viewFunction()
 window.mainloop()
