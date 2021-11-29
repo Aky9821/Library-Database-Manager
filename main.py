5ea4d05668bfa1eaf1f3af86fc195971f6b7234e
Akshay <akshay..nagar0101@outlook.com>
2021-11-29 17:53:56 +0530

    All functions complete and working

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import sqlite3
+import os
 
-conn = sqlite3.connect("lite.db")
-
-cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS Store (Item VARCHAR, Quantity INTEGER, price REAL)")
-cur.execute("INSERT INTO store VALUES ('Wine Glass', 8,10.5)")
-conn.commit()
-conn.close()
+os.system('python GUI.py')
